bb7d3bd7d83235a6a422bf026341901464df181d
Дмитрий <Thedimok@rambler.ru>
2018-08-13 22:28:50 +0300

    Урок 2 доделал методы, все реализовано, есть костыли.

--- src/main/java/lesson2/Start.java
+++ src/main/java/lesson2/Start.java
@@ -5,20 +5,47 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Start {
+
+    private static Scanner sc;
+
     public static void main(String[] args) {
         try {
             SessionData.connect();
             SessionData.clearDB();
             SessionData.addProducts();
 
-            System.out.println("Введи имя продукта: ");
-            Scanner sc = new Scanner(System.in);
-            ResultSet resultSet = SessionData.searchProduct(sc.nextLine());
-            if (!resultSet.next()) {
-                System.out.println("Такого товара нет");
-            } else {
-                while (resultSet.next()) {
-                    System.out.println("цена: " + resultSet.getString(1));
+//            System.out.println("Введи имя продукта: ");
+
+            while (true) {
+                sc = new Scanner(System.in);
+                String string = sc.nextLine();
+                System.out.println(string);
+                if (string.startsWith("/цена")) {
+                    ResultSet resultSet = SessionData.searchProduct(string);
+                    if (resultSet.next() == false) {
+                        System.out.println("Такого товара нет");
+                    } else {
+//                        while (resultSet.next()) {
+                        System.out.println("resultnext true");
+                        System.out.println("цена: " + resultSet.getString(1));
+                    }
+                } else if (string.startsWith("/newprice")) {
+                    SessionData.newPrice(string);
+
+                } else if (string.startsWith("/товарыценой")) {
+                    ResultSet rs = SessionData.searchRangeProducts(string);
+//                    if (rs.next() == false) {
+//                        System.out.println("Таких товаров нет");
+//                    } else {
+                    Boolean flag = false;//todo
+                    while (rs.next()) {
+                        System.out.println("resultnext true");
+                        System.out.println("Найденные товары: " + rs.getString(1));
+                        flag = true;//костыль
+                    }
+                    if (rs.next() == false && flag == false) {
+                        System.out.println("Таких товаров нет");
+                    }
                 }
             }
 
@@ -27,5 +54,6 @@ public class Start {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
