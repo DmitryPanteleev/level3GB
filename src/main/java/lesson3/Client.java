ed9a9af7d135bef58a7e68942fc2587827b0885d
Дмитрий <Thedimok@rambler.ru>
2018-08-16 11:28:05 +0300

    Урок 3

---
+++ src/main/java/lesson3/Client.java
@@ -0,0 +1,9 @@
+package lesson3;
+
+public class Client {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/lesson3/Server.java
@@ -0,0 +1,7 @@
+package lesson3;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
