2829f006957c434ebb2cfa2c401923ea4aa78086
DPanteleev <TheDimok@rambler.ru>
2018-08-16 16:37:20 +0300

    lesson 3 задание 1 и 2 вроде сделал

--- src/main/java/lesson3/Start.java
+++ src/main/java/lesson3/Start.java
@@ -1,9 +1,48 @@
 package lesson3;
 
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Start {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        writeByteMass("src/main/java/lesson3/file1.txt");
+        writeManyMass("src/main/java/lesson3/file2.txt",
+                "src/main/java/lesson3/file3.txt",
+                "src/main/java/lesson3/file4.txt",
+                "src/main/java/lesson3/file5.txt",
+                "src/main/java/lesson3/file6.txt"
+        );
     }
+
+    static void writeByteMass(String path) {
+
+        try (FileInputStream in = new FileInputStream(path)){
+            byte[] arr = new byte[60];
+            int x;
+            while ((x = in.read(arr)) != -1){
+                System.out.println(new String(arr,0,x));
+            };
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    static void writeManyMass(String... path) throws Exception {
+        ArrayList<InputStream> arr = new ArrayList<>();
+        for (int i = 0; i < path.length; i++) {
+            arr.add(new FileInputStream(path[i]));
+        }
+        SequenceInputStream in = new SequenceInputStream(Collections.enumeration(arr));
+        int x;
+        while ((x = in.read()) != -1){
+            System.out.print((char) x);
+        }
+        in.close();
+    }
+
 
 }
